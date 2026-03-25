db8bc241d1993a814d52b9ee2d96e068d1a7064e
sanika-tneuralai <sanika.s@tneuralai.com>
2026-03-25 16:30:00 +0530

    Test: Modified camera-detection-url from edge-server to EC2 for testing

--- services/orchestration/main.py
+++ services/orchestration/main.py
@@ -61,7 +61,9 @@ logger = logging.getLogger(__name__)
 # =============================================================================
 
 # Service URLs
-CAMERA_DETECTION_URL = os.getenv("CAMERA_DETECTION_URL", "http://100.123.244.59:8000")
+
+CAMERA_DETECTION_URL=os.getenv("CAMERA_DETECTION_URL","http://13.201.133.171:8000") #EC2
+# CAMERA_DETECTION_URL = os.getenv("CAMERA_DETECTION_URL", "http://100.123.244.59:8000") #edgeserver
 USECASE_SERVICE_URL = os.getenv("USECASE_SERVICE_URL", "http://3.6.160.230:8001")
 ALERT_SERVICE_URL = os.getenv("ALERT_SERVICE_URL", "http://3.6.160.230:8002")
 
