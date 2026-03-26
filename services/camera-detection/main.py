7cd999e85a0fa5972cfb5250d065f104d92d6e54
sanika-tneuralai <sanika.s@tneuralai.com>
2026-03-26 11:57:29 +0530

    FIX: changes the port for better testing

--- services/camera-detection/main.py
+++ services/camera-detection/main.py
@@ -167,7 +167,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8000,
+        port=8004,
         reload=False,
         log_level="info"
     )
